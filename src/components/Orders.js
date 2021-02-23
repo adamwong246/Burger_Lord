1fb238f3b5784954ce0dfffcc12fb1b302cfecd9
adam wong <adamwong246@gmail.com>
2021-02-23 09:42:17 -0800

    recompute price

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -9,25 +9,36 @@ class Orders extends React.Component {
 
   sandwichNamesOfOrder(order, allSandwiches){
 
-    const orderSandwicheIdss = order.sandwiches
+    const orderSandwicheIds = order.sandwiches
 
-    return orderSandwicheIdss.map((orderSandwicheId) => {
+    return orderSandwicheIds.map((orderSandwicheId) => {
       return allSandwiches.filter((propsSandwich) =>  propsSandwich.id === orderSandwicheId)[0].name
     }).map((name) => {
       return (<li>{name}</li>)
     })
+  }
 
+
+  computedPrice(order, allSandwiches){
+
+    const orderSandwicheIds = order.sandwiches
+
+    return orderSandwicheIds.reduce((mm, orderSandwicheId) => {
+      return mm + allSandwiches.filter((propsSandwich) =>  propsSandwich.id === orderSandwicheId)[0].cost
+    }, 0)
   }
 
   render(props) {
     return (<div>
-      <h2>Orders</h2>
+      <h1>Please complete some orders</h1>
+
       <table>
 
         <tr>
           <th>#</th>
           <th>sandwiches</th>
           <th>status</th>
+          <th>cost</th>
         </tr>
         {
           this.props.orders.map((order) => (
@@ -41,7 +52,12 @@ class Orders extends React.Component {
                 </ul>
                 
               </td>
-              <td>{order.status ? <button onClick={() => this.props.completeOrder(order.id)}> Complete Order</button> : "picked-up"}</td>
+              <td>${
+                this.computedPrice(order, this.props.sandwiches)
+                }</td>
+              <td>{
+                order.status ? <button onClick={() => this.props.completeOrder(order.id)}> Complete Order</button> : "picked-up"}
+              </td>
             </tr>
           ))
         }
